eb238a7de0b7c9d4377137f6b517c422af0649b0
ribeyrro <ribeyro.quispe@tecsup.edu.pe>
2024-07-18 16:54:36 -0500

    agregar con productos

--- src/examples/ProductList.vue
+++ src/examples/ProductList.vue
@@ -1,14 +1,14 @@
 <script lang="ts">
-import { ref } from 'vue';
-import ProductCard from './ProductCard.vue';
+import { ref, type Ref } from 'vue';
 import type { Product } from './Product.ts';
+import ProductCard from './ProductCard.vue';
 
 export default {
     components: {
         ProductCard,
     },
     setup() {
-        const products: Array<Product> = ref([
+        const products: Ref<Product[]> = ref([
             { name: "Chair", price: 100, id:1},
             { name: "Microphone", price: 200, id: 2 },
             { name: "Monitor", price: 300, id: 3}
